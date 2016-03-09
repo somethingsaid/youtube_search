e9897ed8bac81e3385e40a214732d4b6e67120cb
somethingsaid <kwong.billy@gmail.com>
2016-03-09 11:35:44 -0500

    Put together channel and video links from Ids

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-var resource = ["channel", "playlist", "video"];
+var resource = ["channel", "video"]; // playlists are slightly different
 
 // when user submits query
 $(function(){
@@ -8,7 +8,7 @@ $(function(){
         event.preventDefault();
         $("#search-results").empty();
         var searchTerm = $('#query').val();
-        for (var i = 0; i < 3; i++) {
+        for (var i = 0; i < 2; i++) {
             getRequest(searchTerm, i);
         }
     });
@@ -37,18 +37,33 @@ function showResults(results, typeIndex){
     var img = "";
     var descr = "";
     var link = "";
-    $('#search-results').append('<h3>' + resource[typeIndex].toUpperCase() + ' RESULTS</h3>');
+    // Create header for section
+    $('#search-results').append('<h3 class=' + resource[typeIndex] + '>' + resource[typeIndex].toUpperCase() + ' RESULTS</h3>');
     console.log("These are your results for: " + resource[typeIndex]);
+
+    // Iterate and piece together html entry for each video item
     for (var i = 0; i < results.length; i++) {
         descr = results[i].snippet.title;
-        link = "https://www.youtube.com"; // if no channeId, go to playlistId, or videoId, final default to youTube homepage
-        img = "<a target=\'_blank\' class = " + resource[typeIndex] + " href=" + link + "><img id=\'thumbnail" + i + "\' src=" + results[i].snippet.thumbnails.default.url + " width=\'120\' height=\'90\'></a>";
-        html += '<p>' + img + descr + '</p>';
-    }    
+
+        if ((results[i].id.kind == "youtube#channel") && (results[i].id.channelId.length > 1))  {
+            link = "https://www.youtube.com/channel/" + results[i].id.channelId;
+        }
+        else if ((results[i].id.kind == "youtube#video") && (results[i].id.videoId.length > 1)) {
+            link = "https://www.youtube.com/watch?v=" + results[i].id.videoId;
+        }
+        else {
+            results.splice(i, 1); // remove item if there is no ID
+            link = "bad";
+        }
+
+        if (link != "bad") {
+            img = "<a target=\'_blank\' class = " + resource[typeIndex] + " href=" + link + "><img id=\'thumbnail" + i + "\' src=" + results[i].snippet.thumbnails.default.url + " width=\'120\' height=\'90\'></a>";
+            html += '<p>' + img + descr + '</p>';
+        }
+    }
     $('#search-results').append(html);
 }
 
-// run a filter on the items that come back to only display results with videoID
 // play with also next-page-token
 //EOF
 });
